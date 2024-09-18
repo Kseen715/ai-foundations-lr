102b881b470b1130f78f2d22cfe5e1469470b217
kseen <korenevdn@gmail.com>
2024-09-18 23:25:04 +0300

    Finnish

--- main.py
+++ main.py
@@ -197,9 +197,9 @@ def main():
                     copy_solution(best, current)
                 else:
                     rejected += 1
-                    if rejected % 10 == 0:
-                        bad_solutions_x.append(float(time.time() - start_time))
-                        bad_solutions_y.append(rejected)
+                    # if rejected % 10 == 0:
+                    bad_solutions_x.append(float(time.time() - start_time))
+                    bad_solutions_y.append(rejected)
             else:
                 copy_solution(working, current)
         # print(f"Best energy = {best.energy}")
@@ -214,15 +214,21 @@ def main():
             dpg.set_value("series_tag_temp", [temperature_x, temperature_y])
             # dpg.fit_axis_data("y_axis_temp")
             dpg.fit_axis_data("x_axis_temp")
+            dpg.set_value("temp_text", f"{temperature:.2f}")
         if len(best_energy_x) > 0:
             dpg.set_value("series_tag_energy", [best_energy_x, best_energy_y])
             # dpg.fit_axis_data("y_axis_energy")
             dpg.set_axis_limits("y_axis_energy", -0.5, max(best_energy_y) + 0.5)
             dpg.fit_axis_data("x_axis_energy")
+            dpg.set_value("energy_text", f"{best.energy:.2f}")
         if len(bad_solutions_x) > 0:
             dpg.set_value("series_tag_bad", [bad_solutions_x, bad_solutions_y])
             dpg.fit_axis_data("x_axis_bad")
             dpg.set_axis_limits("y_axis_bad", -0.5, max(bad_solutions_y) + 0.5)
+            dpg.set_value("bads_text", f"{bad_solutions_y[-1]}")
+        if len(temperature_x) > 0:
+            dpg.set_value("time_text", f"{temperature_x[-1]:.2f}s")
+        
 
 
 
@@ -255,16 +261,67 @@ def stop():
             pass
     process = None
 
+def update_layout():
+    port_width = dpg.get_viewport_client_width()
+    port_height = dpg.get_viewport_client_height()
+
+    inputs_height = 26
+
+    board_size = min(port_width, port_height - inputs_height * 6)
+    
+    plots_width = port_width - board_size / 0.97
+    plots_height = board_size / 3 - 3 #/ 3.031
+
+    inputs_width = min(max(port_width * 0.15, 100), 500)
+    # inputs_height = min(max(port_height * 0.25, 100), 500)
+
+    outputs_width = min(max(port_width * 0.15, 100), 500)
+    # outputs_height = min(max(port_height * 0.25, 100), 500)
+    
+
+    if dpg.does_item_exist("board"):
+        dpg.configure_item("board", width=board_size, height=board_size)
+    
+    if dpg.does_item_exist("max_temp"):
+        dpg.configure_item("max_temp", width=inputs_width)
+    if dpg.does_item_exist("min_temp"):
+        dpg.configure_item("min_temp", width=inputs_width)
+    if dpg.does_item_exist("alpha"):
+        dpg.configure_item("alpha", width=inputs_width)
+    if dpg.does_item_exist("num_queens"):
+        dpg.configure_item("num_queens", width=inputs_width)
+    if dpg.does_item_exist("steps"):
+        dpg.configure_item("steps", width=inputs_width)
+
+    if dpg.does_item_exist("temp_text"):
+        dpg.configure_item("temp_text", width=inputs_width)
+    if dpg.does_item_exist("energy_text"):
+        dpg.configure_item("energy_text", width=inputs_width)
+    if dpg.does_item_exist("bads_text"):
+        dpg.configure_item("bads_text", width=inputs_width)
+    if dpg.does_item_exist("time_text"):
+        dpg.configure_item("time_text", width=inputs_width)
+
+    if dpg.does_item_exist("plot_temp"):
+        dpg.configure_item("plot_temp", width=plots_width, height=plots_height)
+    if dpg.does_item_exist("plot_energy"):
+        dpg.configure_item("plot_energy", width=plots_width, height=plots_height)
+    if dpg.does_item_exist("plot_bads"):
+        dpg.configure_item("plot_bads", width=plots_width, height=plots_height)
+
+    if dpg.does_item_exist("y_axis_temp"):
+        dpg.set_axis_limits("y_axis_temp", 0, data_cb["init_temp"])
 
 if __name__ == "__main__":
     dpg.create_context()
-    dpg.create_viewport(title='N-Queen Task', width=1000, height=1000)
-
+    dpg.create_viewport(title='N-Queen Task', width=800, height=600)
+    dpg.set_viewport_resize_callback(update_layout)
+    # print(dpg.get_viewport_client_width())
+    # print(dpg.get_viewport_client_height())
 
     width, height, channels, data = dpg.load_image("solution.png")
     with dpg.texture_registry(show=False):
         dpg.add_dynamic_texture(width=width, height=height, default_value=data, tag="texture_tag")
-
 
     with dpg.window(label="Example Window", tag="fullscreen"):
 
@@ -288,73 +345,109 @@ if __name__ == "__main__":
                 dpg.add_theme_style(dpg.mvPlotStyleVar_LineWeight, 3.0, category=dpg.mvThemeCat_Plots)
 
         def on_value_change_max_temp(sender, app_data, user_data):
+            stop()
             user_data['init_temp'] = int(app_data)
             dpg.set_axis_limits("y_axis_temp", 0, data_cb["init_temp"])
 
         def on_value_change_min_temp(sender, app_data, user_data):
+            stop()
             user_data['fin_temp'] = int(app_data)
 
         def on_value_change_alpha(sender, app_data, user_data):
+            stop()
             user_data['alpha'] = int(app_data)
 
         def on_value_change_steps(sender, app_data, user_data):
+            stop()
             user_data['steps_per_change'] = int(app_data)
 
         def on_value_change_num_queens(sender, app_data, user_data):
+            stop()
             user_data['num_queens'] = int(app_data)
             emit_solution_image(None)
             width, height, channels, data = dpg.load_image("solution.png")
             dpg.set_value("texture_tag", data)
 
-
         # Максимальная температура
-        dpg.add_input_float(label="Max temperature", 
-                            default_value=100, 
-                            width=200, 
-                            callback=on_value_change_max_temp, 
-                            user_data=data_cb,
-                            min_value=0.0,
-                            step=1.0)
-        # Минимальная температура
-        dpg.add_input_float(label="Min temperature", 
-                            default_value=0.01, 
-                            width=200, 
-                            callback=on_value_change_min_temp, 
-                            user_data=data_cb,
-                            min_value=0.0,
-                            step=1.0)
-        # Коэффициент понижения температуры
-        dpg.add_input_float(label="Temperature reduction coefficient", 
-                            default_value=0.99, 
-                            width=200, 
-                            callback=on_value_change_alpha, 
-                            user_data=data_cb,
-                            min_value=0.0,
-                            step=1.0)
-        # Количество ферзей
-        dpg.add_input_int(label="Number of queens", 
-                          default_value=8, 
-                          width=200, 
-                          callback=on_value_change_num_queens, 
-                          user_data=data_cb, 
-                          min_value=1, max_value=30)
-        # Количество шагов при постоянном значении температуры
-        dpg.add_input_int(label="Number of steps at constant temperature", 
-                          default_value=10, 
-                          width=200, 
-                          callback=on_value_change_steps, 
-                          user_data=data_cb)
+        with dpg.group(horizontal=True):
+            with dpg.group():
+                dpg.add_input_float(label="Max temperature", 
+                                    default_value=100, 
+                                    width=200, 
+                                    callback=on_value_change_max_temp, 
+                                    user_data=data_cb,
+                                    min_value=0.0,
+                                    step=1.0,
+                                    tag="max_temp")
+                # Минимальная температура
+                dpg.add_input_float(label="Min temperature", 
+                                    default_value=0.01, 
+                                    width=200, 
+                                    callback=on_value_change_min_temp, 
+                                    user_data=data_cb,
+                                    min_value=0.0,
+                                    step=1.0,
+                                    tag="min_temp")
+                # Коэффициент понижения температуры
+                dpg.add_input_float(label="Temperature reduction coefficient", 
+                                    default_value=0.99, 
+                                    width=200, 
+                                    callback=on_value_change_alpha, 
+                                    user_data=data_cb,
+                                    min_value=0.0,
+                                    step=1.0,
+                                    tag="alpha")
+                # Количество ферзей
+                dpg.add_input_int(label="Number of queens", 
+                                default_value=8, 
+                                width=200, 
+                                callback=on_value_change_num_queens, 
+                                user_data=data_cb, 
+                                min_value=1, max_value=30,
+                                tag="num_queens")
+                # Количество шагов при постоянном значении температуры
+                dpg.add_input_int(label="Number of steps at constant temperature", 
+                                default_value=10, 
+                                width=200, 
+                                callback=on_value_change_steps, 
+                                user_data=data_cb,
+                                tag="steps")
+            with dpg.group():
+                # text output
+                dpg.add_input_text(default_value="...", 
+                       readonly=True, 
+                       tag="temp_text", 
+                       width=50)
+                dpg.add_input_text(default_value="...",
+                          readonly=True,
+                          tag="energy_text",
+                          width=50)
+                dpg.add_input_text(default_value="...",
+                            readonly=True,
+                            tag="bads_text",
+                            width=50)
+                dpg.add_input_text(default_value="...",
+                            readonly=True,
+                            tag="time_text",
+                            width=50)
+                
+            with dpg.group():
+                dpg.add_text("Temperature")
+                dpg.add_text("Best Energy")
+                dpg.add_text("Bad Solutions")
+                dpg.add_text("Time")
         
         with dpg.group(horizontal=True):
             dpg.add_button(label="run", callback=run)
             dpg.add_button(label="stop", callback=stop)
+
 
         text_width = dpg.get_viewport_client_width() * 5 / 10
         text_height = dpg.get_viewport_client_width() * 5 / 10
         with dpg.group(horizontal=True):
             dpg.add_image("texture_tag", \
                           width=text_width, \
-                          height=text_height)
+                          height=text_height, tag="board")
 
             emit_solution_image(None)
             width, height, channels, data = dpg.load_image("solution.png")
@@ -365,7 +458,8 @@ if __name__ == "__main__":
             with dpg.group():
                 with dpg.plot(label="Temperature", 
                               width=plots_width,
-                              height=plots_height):
+                              height=plots_height,
+                              tag="plot_temp"):
                     dpg.add_plot_legend()
                     dpg.add_plot_axis(dpg.mvXAxis, label="time, s", tag="x_axis_temp")
                     dpg.add_plot_axis(dpg.mvYAxis, label="temperature", tag="y_axis_temp")
@@ -374,7 +468,8 @@ if __name__ == "__main__":
                 dpg.bind_item_theme("series_tag_temp", "plot_theme_temp")
                 with dpg.plot(label="Best Energy", 
                               width=plots_width,
-                              height=plots_height):
+                              height=plots_height,
+                              tag="plot_energy"):
                     dpg.add_plot_legend()
                     dpg.add_plot_axis(dpg.mvXAxis, label="time, s", tag="x_axis_energy")
                     dpg.add_plot_axis(dpg.mvYAxis, label="energy", tag="y_axis_energy")
@@ -382,12 +477,15 @@ if __name__ == "__main__":
                 dpg.bind_item_theme("series_tag_energy", "plot_theme_energy")
                 with dpg.plot(label="Bad Solutions", 
                               width=plots_width,
-                              height=plots_height):
+                              height=plots_height,
+                              tag="plot_bads"):
                     dpg.add_plot_legend()
                     dpg.add_plot_axis(dpg.mvXAxis, label="time, s", tag="x_axis_bad")
                     dpg.add_plot_axis(dpg.mvYAxis, label="bad solutions", tag="y_axis_bad")
                     dpg.add_line_series(bad_solutions_x, bad_solutions_y, parent="y_axis_bad", tag="series_tag_bad")
                 dpg.bind_item_theme("series_tag_bad", "plot_theme_bads")
+
+    update_layout()
 
     dpg.setup_dearpygui()
     dpg.set_primary_window("fullscreen", True)
